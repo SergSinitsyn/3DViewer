26ba869c1a46a117d4d60376fcb27c72bff3c59a
Bernarda Verla <bernarda@in-d3.nsk.21-school.ru>
2023-06-13 15:00:22 +0700

    add makefile target install is working, fix cmake for mac

--- src/main.cc
+++ src/main.cc
@@ -7,14 +7,14 @@
 int main(int argc, char* argv[]) {
   QApplication application(argc, argv);
   // Make a "deep copy" of current locale name.
-  std::string prev_loc = std::setlocale(LC_ALL, nullptr);
-  const char* loc = std::setlocale(LC_ALL, "en_US.UTF-8");
+  std::string prev_loc = setlocale(LC_ALL, nullptr);
+  const char* loc = setlocale(LC_ALL, "en_US.UTF-8");
   MainWindow view;
   s21::VModel model;
   Controller controller(model, view);
   view.show();
   int res = application.exec();
   // Restore the previous locale.
-  std::setlocale(LC_ALL, prev_loc.c_str());
+  setlocale(LC_ALL, prev_loc.c_str());
   return res;
 }
