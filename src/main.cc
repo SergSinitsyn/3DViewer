9b7897bb7cbfbc4ef59881def54ae31076d24d04
Joellebo <joellebo@student.21-school.ru>
2023-06-16 16:44:24 +0700

    linux fixes

--- src/main.cc
+++ src/main.cc
@@ -6,15 +6,11 @@
 
 int main(int argc, char* argv[]) {
   QApplication application(argc, argv);
-  // Make a "deep copy" of current locale name.
-  std::string previous_locale = setlocale(LC_ALL, nullptr);
-  const char* loc = setlocale(LC_ALL, "en_US.UTF-8");
+  setlocale(LC_ALL, "C");
   s21::MainWindow view;
   s21::VModel model;
   s21::Controller controller(model, view);
   view.show();
   int result = application.exec();
-  // Restore the previous locale.
-  setlocale(LC_ALL, previous_locale.c_str());
   return result;
 }
