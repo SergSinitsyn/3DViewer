cab537f7e1a98dda723d6d2c178967264717f210
Serge <joellebo@student.21-school.ru>
2023-06-21 16:16:10 +0700

    decorator added
    Former-commit-id: d8e4286ea5334dbd532dbfebc89bc44002b57e0a

---
+++ src/other/memento.h
@@ -0,0 +1,31 @@
+#ifndef VIEWER2_OTHER_MEMENTO_H
+#define VIEWER2_OTHER_MEMENTO_H
+
+// Pattern Xranitel
+template <class T>
+class Memento {
+ public:
+  Memento(){};
+  explicit Memento(const T& s) : is_valid_{true}, state_{s} {};
+  explicit Memento(const T* s) : is_valid_{true} {
+    if (s != nullptr)
+      state_ = *s;
+    else
+      is_valid_ = false;
+  };
+  T const* GetState() const {
+    if (!is_valid_) throw std::runtime_error("Invalid state!");
+    return &state_;
+  };
+  void SetState(const T* new_state) {
+    if (new_state != nullptr && &state_ != new_state) {
+      state_ = *new_state;
+      is_valid_ = true;
+    }
+  };
+
+ protected:
+  T state_;
+  bool is_valid_ = false;
+};
+#endif  // VIEWER2_OTHER_MEMENTO_H
